411cbe5eb3b310355f04a5a1b39e3b5873d815fe
Oguz Huner <oguzcanhuner@gmail.com>
2013-10-13 16:33:13 +0100

    check for empty term and render nothing

--- app.rb
+++ app.rb
@@ -5,12 +5,12 @@ require_relative 'lib/tumblr'
 CONSUMER_KEY = "HOkdE4AtmCYUqthdKsJsSzlQH47n6UGwXqs5B5r838KMLwWt40"
 
 get "/" do
-  if params[:term]
+  if params[:term].nil? || params[:term].empty?
+    @images = []
+  else
     tumblr = Tumblr.new(key: CONSUMER_KEY)
     posts = tumblr.fetch_tagged_posts(tag: params[:term], limit: 20)
     @images = posts.collect(&:photos).flatten
-  else
-    @images = []
   end
 
   haml :index
